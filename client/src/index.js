33879f46894c6de5c9e219f0cc0fc6af6dab7d89
srt1104 <ragitss1104@gmail.com>
2023-07-28 22:59:24 +0530

    setup api and configure it with redux-toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "state/api";
 
 import "./index.css";
 import App from "./App";
@@ -10,8 +12,11 @@ import globalReducer from "state";
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
