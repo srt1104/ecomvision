d0d03bbc5884556361a744de9de1dd08e6c7551c
srt1104 <ragitss1104@gmail.com>
2023-07-30 20:15:55 +0530

    add geography page functionality

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,3 +1,5 @@
+import getCountryIso3 from "country-iso-2-to-3";
+
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
@@ -69,6 +71,32 @@ export const getTransactions = async (req, res) => {
       transactions,
       total,
     });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      ++acc[countryISO3];
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
